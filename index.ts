6c9d4ef0e25598cc40e86abd4a43ad60b7c1445d
shumailagithub <163028638+shumailagithub@users.noreply.github.com>
2024-04-14 00:58:43 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let roundNumber: number = Math.floor((Math.random() * 5) + 1);
+
+let userInput = await inquirer.prompt({
+    type: "number",
+    name: "GuesseNumber",
+    message: "Guess a Number between 1 till 5 ",
+})
+
+if(userInput.GuesseNumber === roundNumber){
+    console.log("Congatulations! your guess number is Correct!");
+    
+}else{
+    console.log(`Sorry! try one more time. Correct answer is: ${roundNumber}`);
+    
+}
